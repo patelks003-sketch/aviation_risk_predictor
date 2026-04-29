348bf5998602cd19d34e2b702fd3059a986a78eb
patelks003-sketch <patelks003@gmail.com>
2026-04-29 07:00:14 +0100

    Updated risk logic

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-ast.set_page_config(page_title="Aviation Risk Predictor")
+# Page setup
+st.set_page_config(page_title="Aviation Risk Predictor")
 
+# Title
 st.title("✈️ Aviation Risk Predictor")
 
-st.write("This app predicts aviation risk level (Demo version)")
+# Description
+st.write("This app predicts aviation risk level")
 
-altitude = st.number_input("Enter altitude (feet)")
+# Inputs
+altitude = st.number_input("Enter altitude (feet)", min_value=0)
 weather = st.selectbox("Weather condition", ["Clear", "Cloudy", "Storm"])
 
+# Button
 if st.button("Predict Risk"):
+
 if altitude > 30000 and weather == "Storm":
 st.error("High Risk ✈️⚠")
+
 elif altitude > 20000:
 st.warning("Medium Risk ⚠")
+
 else:
 st.success("Low Risk ✅")
